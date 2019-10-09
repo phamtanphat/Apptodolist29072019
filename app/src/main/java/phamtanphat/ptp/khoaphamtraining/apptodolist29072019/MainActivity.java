0562b9947fe6d873b857f27cdcf0a87285a03d1b
phatdroid94 <phatdroid94@gmail.com>
2019-10-09 18:44:33 +0700

    setup ui dang ky

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/apptodolist29072019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/apptodolist29072019/MainActivity.java
@@ -3,6 +3,12 @@ package phamtanphat.ptp.khoaphamtraining.apptodolist29072019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+
+import org.w3c.dom.Text;
+
+import java.util.regex.Pattern;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +18,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //regex
+
+//        Patterns.EMAIL_ADDRESS.matcher();
+//        Patterns.EMAIL_ADDRESS.matcher();
     }
 }

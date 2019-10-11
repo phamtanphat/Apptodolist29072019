3619fb99fab0e7eb6371114a572571393e77e9a4
phatdroid94 <phatdroid94@gmail.com>
2019-10-11 18:33:07 +0700

    repository

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/apptodolist29072019/api/retrofit/ApiRequest.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/apptodolist29072019/api/retrofit/ApiRequest.java
@@ -1,11 +1,18 @@
 package phamtanphat.ptp.khoaphamtraining.apptodolist29072019.api.retrofit;
 
+import phamtanphat.ptp.khoaphamtraining.apptodolist29072019.api.response.LoginResponse;
 import retrofit2.Call;
-import retrofit2.http.GET;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
 
 public interface ApiRequest {
 
+    // ctrl + alt + o
+    @FormUrlEncoded
+    @POST("register.php")
+    Call<LoginResponse> onLoginResult(
+                                    @Field("username") String username,
+                                    @Field("password") String password);
 
-    @GET("connect.php")
-    Call<String> getConnect();
 }
---
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/apptodolist29072019/respository/LoginRepository.java
@@ -0,0 +1,48 @@
+package phamtanphat.ptp.khoaphamtraining.apptodolist29072019.respository;
+
+import android.database.Observable;
+
+import androidx.lifecycle.MutableLiveData;
+
+import phamtanphat.ptp.khoaphamtraining.apptodolist29072019.api.response.LoginResponse;
+import phamtanphat.ptp.khoaphamtraining.apptodolist29072019.api.retrofit.ApiRequest;
+import phamtanphat.ptp.khoaphamtraining.apptodolist29072019.api.retrofit.RetrofitInit;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class LoginRepository {
+
+    private static LoginRepository repository = null;
+    private ApiRequest apiRequest;
+    private LoginRepository() {
+        apiRequest = RetrofitInit.initApi();
+    }
+//
+    public static LoginRepository getInstance(){
+        if (repository == null){
+            repository = new LoginRepository();
+        }
+        return repository;
+    }
+
+
+    public MutableLiveData<LoginResponse> checkLogin(String username , String password){
+        final MutableLiveData<LoginResponse> loginResponse = new MutableLiveData<>();
+        apiRequest.onLoginResult(username , password).enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                loginResponse.postValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+
+            }
+        });
+        return loginResponse;
+    }
+
+
+
+}
